26889d2f32eea5944d2a6e0aa3d180fb9c05bd0c
暇な人 <yuuta4083@gmail.com>
2025-03-01 13:44:04 +0900

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sgc.py',
-    version="0.0.1",
+    version="0.0.2",
     author='Mochagod123',
     url="https://github.com/mochagod123/sgc.py",
     license='GPL 3.0 license',
